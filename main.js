93f15b522e534d6aa934066571f036d894e6382c
AntoRin <antorinish@gmail.com>
2022-06-01 14:47:27 +0530

    Add prompts; fix bug where a function was not called with the correct args

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
-const fetch = require("node-fetch");
 const { spawn } = require("child_process");
+const fetch = require("node-fetch");
+const inquirer = require("inquirer");
+const { existsSync } = require("fs");
 
 async function hasBookingOpened(bookMyShowURL) {
    return !!(await fetch(bookMyShowURL, {
       redirect: "error",
-   }).catch(() => false));
+   }).catch(e => {
+      if (e.type === "no-redirect") return false;
+      else throw e;
+   }));
 }
 
 async function poll(interval, URL) {
@@ -12,31 +17,60 @@ async function poll(interval, URL) {
       const result = await hasBookingOpened(URL);
       console.log(result);
       if (result) {
-         spawn("start", ["alarm.mp4"], {
+         return;
+      } else {
+         await new Promise(resolve => {
+            setTimeout(resolve, interval);
+         });
+         await poll(interval, URL);
+      }
+   } catch (error) {
+      console.error(error);
+      process.exit(1);
+   }
+}
+
+inquirer
+   .prompt([
+      {
+         type: "input",
+         name: "bookMyShowURL",
+         message:
+            "Enter a valid bookmyshow URL for a theatre you'd like to poll for availability:",
+         validate: input =>
+            input && input.startsWith("https://in.bookmyshow.com")
+               ? true
+               : "Must provide a valid bookmyshow URL",
+      },
+      {
+         type: "input",
+         name: "pollInterval",
+         message: "Enter the interval for polling in milliseconds:",
+         validate: input => (!isNaN(Number(input)) ? true : "Must enter a number"),
+         default: 5000,
+      },
+      {
+         type: "input",
+         name: "alarm",
+         message:
+            "File that will be opened for notifying once booking has been opened (leave it blank to skip):",
+         validate: input =>
+            !input ? true : existsSync(input) ? true : "Must be a valid file system path",
+      },
+   ])
+   .then(answers => {
+      poll(answers.pollInterval, answers.bookMyShowURL).then(() => {
+         if (!answers.alarm) return;
+         spawn("start", [answers.alarm], {
             detached: true,
             shell: true,
             stdio: ["ignore", "ignore", "ignore"],
          }).on("error", e => {
             console.log(e);
          });
-         return;
-      } else {
-         await new Promise(resolve => {
-            setTimeout(resolve, interval);
-         });
-         poll(interval);
-      }
-   } catch (error) {
-      console.log(error);
-   }
-}
-
-const bookMyShowURL = process.argv[2];
-const interval = process.argv[3] || 5000;
-
-if (!bookMyShowURL || !bookMyShowURL.startsWith("https://in.bookmyshow.com")) {
-   console.error("Must provide a valid bookmyshow URL");
-   process.exit(1);
-}
-
-poll(interval, bookMyShowURL);
+      });
+   })
+   .catch(e => {
+      console.error(e);
+      process.exit(1);
+   });
